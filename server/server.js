ccab9f9e2143fd7778db2551fc6bd0d93726542d
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-02-22 00:51:18 -0800

    Implement basic user query API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const fs = require('fs');
+const {ApolloServer} = require('apollo-server-express')
+
+const userDB = [
+  {
+    userID: 1,
+    username: 'patrick',
+    email: 'patrick@gmail.com',
+    password: 'password',
+  },
+  {
+    userID: 2,
+    username: 'test',
+    email: 'test@gmail.com',
+    password: 'test',
+  }
+];
+
+const resolvers = {
+  Query: {
+    getUsers,
+  },
+}
+
+function getUsers() {
+  return userDB;
+}
+
 const app = express();
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+  resolvers
+});
 
 app.use('/', express.static('public'));
+server.applyMiddleware({app, path:'/graphql'});
 app.listen(3000, () => console.log('Forces server started on port 3000...'));
