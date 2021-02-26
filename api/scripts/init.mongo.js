a91bdd224ec266416031c84612ce645a06168b91
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-02-26 01:09:03 -0800

    Lint backend

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,13 +1,16 @@
+/* global db print */
+/* eslint no-restricted-globals: "off" */
+
 db.users.remove({});
 
 const initialUsers = [
   {
     userID: 1,
-    username: "patrick",
-    email: "patrickjohnsilvestre@gmail.com",
-    password: "$2b$10$LkSc.1gmK1/DyElaP7c43.DRwC0/YtBMSkeLiAD8p6cQbvpZTjAdi"
-  }
+    username: 'patrick',
+    email: 'patrickjohnsilvestre@gmail.com',
+    password: '$2b$10$LkSc.1gmK1/DyElaP7c43.DRwC0/YtBMSkeLiAD8p6cQbvpZTjAdi',
+  },
 ];
 
 db.users.insertMany(initialUsers);
-print(`Inserted ${db.users.count()} users`)
+print(`Inserted ${db.users.count()} users`);
--- api/server.js
+++ api/server.js
@@ -3,14 +3,28 @@ require('dotenv').config();
 const bcrypt = require('bcrypt');
 const express = require('express');
 const fs = require('fs');
-const {ApolloServer} = require('apollo-server-express');
-const {MongoClient} = require('mongodb');
+const { ApolloServer } = require('apollo-server-express');
+const { MongoClient } = require('mongodb');
 
 const saltRounds = 10;
-const mongoURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}` +
-  `@forces.wm1qz.mongodb.net/forces?retryWrites=true&w=majority`;
+const mongoURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}`
+  + '@forces.wm1qz.mongodb.net/forces?retryWrites=true&w=majority';
 
 let db;
+
+async function getUsers() {
+  return db.collection('users').find({}).toArray();
+}
+
+async function registerUser(_, { user }) {
+  const newUser = { ...user };
+
+  newUser.password = await bcrypt.hash(user.password, saltRounds);
+
+  const result = await db.collection('users').insertOne(newUser);
+
+  return db.collection('users').findOne({ _id: result.insertId });
+}
 
 const resolvers = {
   Query: {
@@ -19,54 +33,35 @@ const resolvers = {
 
   Mutation: {
     registerUser,
-  }
-}
-
-async function getUsers() {
-  return await db.collection('users').find({}).toArray();
-}
-
-async function registerUser(_, {user}) {
-  await bcrypt.hash(user.password, saltRounds, async (err, hash) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    user.password = hash;
-    await db.collection('users').insertOne(user);
-  })
-
-  return await db.collection('users').findOne({'username': user.username});
-}
+  },
+};
 
 const app = express();
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
   resolvers,
-  formatError: error => {
+  formatError: (error) => {
     console.log(error);
     return error;
-  }
+  },
 });
 
 async function connectToDb() {
-  const client = new MongoClient(mongoURL, {useNewUrlParser: true});
+  const client = new MongoClient(mongoURL, { useNewUrlParser: true });
   await client.connect();
-  console.log(`Connected to MongoDB`);
+  console.log('Connected to MongoDB');
   db = client.db();
 }
 
-server.applyMiddleware({app, path: '/graphql'});
+server.applyMiddleware({ app, path: '/graphql' });
 
 const port = process.env.API_SERVER_PORT || 3000;
 
 (async () => {
   try {
     await connectToDb();
-    app.listen(port, () =>
-      console.log(`Forces API server started on port ${port}...`));
+    app.listen(port, () => console.log(`Forces API server started on port ${port}...`));
   } catch (err) {
     console.log(`Error: ${err}`);
   }
