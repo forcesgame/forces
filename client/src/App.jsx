69f515998d85ba9c52a0681a8f7b4260fdcae5dd
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-03-11 02:39:11 -0800

    Add home, login, builder placeholders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
+import Builder from './pages/Builder';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/builder" component={Builder} />
+        <Route path="/error" component={Error} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Error() {
+  return (
+    <h1>Sorry, an error occurred. Please try again.</h1>
+  );
+}
