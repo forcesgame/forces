56fbd059840175b510e9d713128c6bdda64500b8
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-03-21 16:59:50 -0700

    Started styling of NavBar.jsx and Home.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {
+  Navbar,
+  Nav,
+} from 'react-bootstrap';
 import AuthNav from './AuthNav';
 
 const NavBar = () => (
-  <nav>
+  <nav bg="light" expand="lg">
+    <Navbar.Brand href="/">Forces</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/builder">Force Builder</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
     <AuthNav />
   </nav>
 );
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 import { useAuth0 } from '@auth0/auth0-react';
 
+import {
+  Container,
+  Jumbotron,
+  Row,
+  Col,
+} from 'react-bootstrap';
+
 export default function Home() {
   const { user } = useAuth0();
 
@@ -17,6 +24,36 @@ export default function Home() {
   }
 
   return (
-    <h1>Welcome to Forces! Please log in.</h1>
+    <div>
+      <Container>
+        <Jumbotron>
+          <h1 style={{ textAlign: 'center' }}>Welcome to Forces! Please log in.</h1>
+        </Jumbotron>
+      </Container>
+      <Container>
+        <Jumbotron>
+          <h2>Description</h2>
+        </Jumbotron>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <p>Game Play Image: 1</p>
+            </Jumbotron>
+          </Col>
+          <Col>
+            <Jumbotron>
+              <p>Game Play Image: 2</p>
+            </Jumbotron>
+          </Col>
+          <Col>
+            <Jumbotron>
+              <p>Game Play Image: 3</p>
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
