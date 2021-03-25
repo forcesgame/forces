a69f28bc48ca4c58d93552696ca0c8b5a0859fb8
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-03-24 19:08:53 -0700

    Basic Force Builder UI structure started

--- client/src/views/Builder.jsx
+++ client/src/views/Builder.jsx
@@ -1,7 +1,91 @@
 import React from 'react';
+import {
+  Col,
+  Container,
+  Jumbotron,
+  Row,
+  Card,
+} from 'react-bootstrap';
 
 export default function Builder() {
   return (
-    <h1>builder placeholder</h1>
+    <div>
+      <h2>Force name</h2>
+      <Container>
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col>
+                <Card>
+                  <p>Unit 1</p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <p>Unit 2</p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <p>Unit 3</p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <p>Unit 4</p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <p>Unit 5</p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <p>Unit 6</p>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </Container>
+      <h2>Available Units</h2>
+      <Container>
+        <Container>
+          <Row>
+            <Col>
+              <Card>
+                <p>Unit 1</p>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <p>Unit 2</p>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <p>Unit 3</p>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <p>Unit 4</p>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <p>Unit 5</p>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <p>Unit 6</p>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </div>
   );
 }
