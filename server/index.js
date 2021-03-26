831a50995f5e15b3d76dfa0ea5c42c1451b2fb96
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-03-26 01:56:28 -0700

    Demo API request handling

--- server/index.js
+++ server/index.js
@@ -1,1 +1,46 @@
-console.log('Hello, world!');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+/*
+serve API requests
+for now, these are just examples of the main methods GET, POST, PUT, DELETE
+ */
+app.get('/api', (req, res) => {
+  res.json({ request: 'GET /api' });
+});
+
+app.post('/api', (req, res) => {
+  const input = req.body;
+  res.json({
+    request: 'POST /api',
+    input,
+  });
+});
+
+app.put('/api', (req, res) => {
+  const input = req.body;
+  res.json({
+    request: 'PUT /api',
+    input,
+  });
+});
+
+app.delete('/api', (req, res) => {
+  res.json({ request: 'DELETE /api' });
+});
+
+// reroute remaining requests to client React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve((__dirname, '../client/build', 'index.html')));
+});
+
+app.listen(5000, () => {
+  // eslint-disable-next-line no-console
+  console.log('API server listening on port 5000...');
+});
