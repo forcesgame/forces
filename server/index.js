a7eb27ccf4a3e25b6b826c2b1180f77efdc7a6ca
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-03-31 03:09:07 -0700

    Fix syntax error

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use('/api', users);
 
 // reroute remaining requests to client React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve((__dirname, '../client/build', 'index.html')));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 // connect to MongoDB
