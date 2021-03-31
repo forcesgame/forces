a6a88971017dd2f9f62f76fe9c7ec8c4862632dc
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-03-30 22:24:25 -0700

    Remove example API routes

--- server/index.js
+++ server/index.js
@@ -2,43 +2,20 @@ const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+
+// include router modules
 const users = require('./routes/users');
 
 dotenv.config();
 const app = express();
+
+// parse incoming requests with JSON payloads
 app.use(express.json());
 
 // serve static files
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-/*
-serve API requests
-for now, these are just examples of the main methods GET, POST, PUT, DELETE
- */
-app.get('/api', (req, res) => {
-  res.json({ request: 'GET /api' });
-});
-
-app.post('/api', (req, res) => {
-  const input = req.body;
-  res.json({
-    request: 'POST /api',
-    input,
-  });
-});
-
-app.put('/api', (req, res) => {
-  const input = req.body;
-  res.json({
-    request: 'PUT /api',
-    input,
-  });
-});
-
-app.delete('/api', (req, res) => {
-  res.json({ request: 'DELETE /api' });
-});
-
+// mount router modules
 app.use('/api', users);
 
 // reroute remaining requests to client React app
