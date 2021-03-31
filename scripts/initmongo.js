08d40534f6fedf49bece182ec9da6e8f6bdc81c8
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-03-30 21:05:58 -0700

    Update database initialization script
    Add default users, forces

--- scripts/initmongo.js
+++ scripts/initmongo.js
@@ -1,27 +1,47 @@
-
-const { MongoClient } = require('mongodb');
-const url = mongodb+srv://db_init:12345@forces.wm1qz.mongodb.net/users?retryWrites=true';
+/*
+Run using the mongo shell:
+mongo mongodb+srv://forces.wm1qz.mongodb.net/forces scripts/initmongo.js --username <username>
+ */
 
 db.users.remove({});
+db.forces.remove({});
 
-const usersDB = [
-    {
-        userID: 1,
-        username: 'patrick',
-        email: 'patrickjohnsilvestre@gmail.com',
-    },
-    {
-        userID: 2,
-        username: 'jesus',
-        email: 'mrjfuentes20@gmail.com',
-    },
-    {
-        userID: 3,
-        username: 'ben',
-        email: 'benjamin.w.lee@sjsu.edu',
-    }
+const users = [
+  { username: 'ben' },
+  { username: 'jesus' },
+  { username: 'patrick' },
 ];
 
-db.users.insertMany(usersDB);
-const count = db.users.count();
-print('Inserted', count, 'users');
+db.users.insertMany(users);
+db.users.createIndex({ username: 1 }, { unique: true });
+const userCount = db.users.count();
+print(`Inserted ${userCount} users`);
+
+const benID = db.users.find({ username: 'ben' }, { _id: 1 });
+const jesusID = db.users.find({ username: 'jesus' }, { _id: 1 });
+const patrickID = db.users.find({ username: 'patrick' }, { _id: 1 });
+
+const forces = [
+  {
+    userID: benID._id,
+    activeUnits: { infantry: 0, bazooka: 0, tank: 0 },
+    reserveUnits: { infantry: 3, bazooka: 3, tank: 2 },
+    units: [],
+  },
+  {
+    userID: jesusID._id,
+    activeUnits: { infantry: 0, bazooka: 0, tank: 0 },
+    reserveUnits: { infantry: 3, bazooka: 3, tank: 2 },
+    units: [],
+  },
+  {
+    userID: patrickID._id,
+    activeUnits: { infantry: 0, bazooka: 0, tank: 0 },
+    reserveUnits: { infantry: 3, bazooka: 3, tank: 2 },
+    units: [],
+  },
+];
+
+db.forces.insertMany(forces);
+const forceCount = db.forces.count();
+print(`Inserted ${forceCount} forces`);
