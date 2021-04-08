05f0f2508a383dcf0d78a39698f39aab5b90cb88
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-08 02:41:52 -0700

    Add axios boilerplate

--- client/src/views/Builder.jsx
+++ client/src/views/Builder.jsx
@@ -1,91 +1,39 @@
-import React from 'react';
-import {
-  Col,
-  Container,
-  Jumbotron,
-  Row,
-  Card,
-} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
 export default function Builder() {
+  const { user } = useAuth0();
+  const [username, setUsername] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [force, setForce] = useState(null);
+
+  async function getForce() {
+    try {
+      if (username === '') return;
+      const response = await axios.get(`/api/forces/${username}`);
+      setForce(response.data);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      const usernameKey = `${process.env.REACT_APP_AUTH0_NAMESPACE}username`;
+      setUsername(user[usernameKey]);
+    }
+    getForce();
+  });
+
+  if (force == null) {
+    return <h1>Loading</h1>;
+  }
+
   return (
-    <div>
-      <h2>Force name</h2>
-      <Container>
-        <Jumbotron>
-          <Container>
-            <Row>
-              <Col>
-                <Card>
-                  <p>Unit 1</p>
-                </Card>
-              </Col>
-              <Col>
-                <Card>
-                  <p>Unit 2</p>
-                </Card>
-              </Col>
-              <Col>
-                <Card>
-                  <p>Unit 3</p>
-                </Card>
-              </Col>
-              <Col>
-                <Card>
-                  <p>Unit 4</p>
-                </Card>
-              </Col>
-              <Col>
-                <Card>
-                  <p>Unit 5</p>
-                </Card>
-              </Col>
-              <Col>
-                <Card>
-                  <p>Unit 6</p>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </Jumbotron>
-      </Container>
-      <h2>Available Units</h2>
-      <Container>
-        <Container>
-          <Row>
-            <Col>
-              <Card>
-                <p>Unit 1</p>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <p>Unit 2</p>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <p>Unit 3</p>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <p>Unit 4</p>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <p>Unit 5</p>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <p>Unit 6</p>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </Container>
-    </div>
+    <>
+      <h1>builder</h1>
+    </>
   );
 }
