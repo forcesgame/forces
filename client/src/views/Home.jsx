2c45277be3f5985f1a8849935e2f41059ad37dcb
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-11 03:57:58 -0700

    Use cards in place of repeated jumbotrons

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,80 +2,67 @@ import React from 'react';
 
 import { useAuth0 } from '@auth0/auth0-react';
 
-import {
-  Container,
-  Jumbotron,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
 
 export default function Home() {
   const { user } = useAuth0();
 
   if (user) {
-    const usernameKey = `${process.env.REACT_APP_AUTH0_NAMESPACE}username`;
-    const username = user[usernameKey];
     return (
-      <div>
-        <Container>
-          <Jumbotron>
-            <h1 style={{ textAlign: 'center' }}>
-              Welcome to Forces,
-              {' '}
-              {username}
-              !
-            </h1>
-          </Jumbotron>
-        </Container>
-        <Container>
-          <Row>
-            <Col>
-              <Jumbotron>
-                <p style={{ textAlign: 'center' }}>Versus</p>
-              </Jumbotron>
-            </Col>
-            <Col>
-              <Jumbotron>
-                <p style={{ textAlign: 'center' }}>Force Builder</p>
-              </Jumbotron>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Link href="#">Versus</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Link href="/builder">Force Builder</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 
   return (
-    <div>
-      <Container>
-        <Jumbotron>
-          <h1 style={{ textAlign: 'center' }}>Welcome to Forces! Please log in.</h1>
-        </Jumbotron>
-      </Container>
-      <Container>
-        <Jumbotron>
-          <h2 style={{ textAlign: 'center' }}>Description</h2>
-        </Jumbotron>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <Jumbotron>
-              <p style={{ textAlign: 'center' }}>Game Play Image: 1</p>
-            </Jumbotron>
-          </Col>
-          <Col>
-            <Jumbotron>
-              <p style={{ textAlign: 'center' }}>Game Play Image: 2</p>
-            </Jumbotron>
-          </Col>
-          <Col>
-            <Jumbotron>
-              <p style={{ textAlign: 'center' }}>Game Play Image: 3</p>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container className="mt-5">
+      <Jumbotron>
+        <h1>Forces</h1>
+        <p>A Browser-Based Multiplayer Tactical Turn-Based Game</p>
+      </Jumbotron>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img src="#" alt="gameplay-image-1" />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img src="#" alt="gameplay-image-2" />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img src="#" alt="gameplay-image-3" />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
