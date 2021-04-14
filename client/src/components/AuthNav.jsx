6ab541fc64cc38262fb2277284a0640cd09caede
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-14 00:25:25 -0700

    Simplify navbar
    Only display login/logout button, change brand to homepage link

--- client/src/components/AuthNav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import AuthenticationButton from './AuthenticationButton';
-
-const AuthNav = () => (
-  <AuthenticationButton />
-);
-
-export default AuthNav;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-} from 'react-bootstrap';
-import AuthNav from './AuthNav';
+import Navbar from 'react-bootstrap/Navbar';
+import AuthenticationButton from './AuthenticationButton';
 
 const NavBar = () => (
-  <Navbar bg="light">
+  <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="/">
-      <img
-        src="#"
-        alt="Forces Logo"
-      />
-      {' '}
       Forces
     </Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
-      <Nav className="mr-auto">
-        <NavDropdown
-          title={(
-            <div>
-              <img
-                src="#"
-                alt="Dropdown Menu"
-              />
-            </div>
-          )}
-          id="basic-nav-dropdown"
-        >
-          <NavDropdown.Item href="/builder">Builder</NavDropdown.Item>
-          <AuthNav />
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
+    <AuthenticationButton />
   </Navbar>
 );
 
