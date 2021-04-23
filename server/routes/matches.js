ed8e0533deb56dbc4ed07df6da1bc381d1fee773
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-23 06:40:58 -0700

    Add POST /matches endpoint

--- server/routes/matches.js
+++ server/routes/matches.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const Match = require('../models/Match');
+const Utilities = require('./database');
 
 /**
  * Gets all matches
@@ -44,7 +45,7 @@ router.get('/matches/:id', async (req, res) => {
 });
 
 /**
- * Gets all matches associated with a user
+ * Gets match associated with a user
  */
 router.get('/matches/users/:userID', async (req, res) => {
   try {
@@ -53,6 +54,45 @@ router.get('/matches/users/:userID', async (req, res) => {
         { user1: req.params.userID }, { user2: req.params.userID }],
     });
 
+    res.send(match);
+  } catch (error) {
+    res.status(400);
+    res.send({
+      type: 'https://forcesgame.com/probs/unspecified-problem',
+      title: 'Unspecified problem',
+      error,
+    });
+  }
+});
+
+/**
+ * Adds a match
+ * If either user1 or user2 are already in a match, no changes will be made
+ * Request body must contain the following fields:
+ * {
+ *   user1ID: <insert-id-here>,
+ *   user2ID: <insert-id-here>
+ * }
+ */
+router.post('/matches', async (req, res) => {
+  try {
+    const user1Match = await Match.findOne({
+      $or: [
+        { user1: req.body.user1ID }, { user2: req.body.user1ID }],
+    });
+
+    const user2Match = await Match.findOne({
+      $or: [
+        { user1: req.body.user2ID }, { user2: req.body.user2ID }],
+    });
+
+    if (user1Match || user2Match) {
+      res.end();
+      return;
+    }
+
+    await Utilities.generateMatch(req.body.user1ID, req.body.user2ID);
+    const match = await Match.findOne({ user1: req.body.user1ID });
     res.send(match);
   } catch (error) {
     res.status(400);
